4aca2afc7e2e9d460d6dd2917416b4fb0a58b45e
Featthima Bou <featthimabou@Featthimas-MacBook-Pro.local>
2023-10-18 16:02:17 +0700

    test navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import About from "./components/About";
-import Experience from "./components/Experience";
 import Navbar from "./components/Navbar";
-import Project from "./components/Project";
 
 function App() {
   return (
     <div>
       <Navbar />
-
-      <div>
-        <About />
-      </div>
-
-      <div>
-        <Experience />
-      </div>
-
-      <div>
-        <Project />
-      </div>
     </div>
   );
 }
