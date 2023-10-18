be3ce7f62d378ef14688bb77bbab4decbc96e259
Featthima Bou <featthimabou@Featthimas-MacBook-Pro.local>
2023-10-18 16:20:37 +0700

    fix main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import Navbar from "./components/Navbar";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Project from "./components/Project";
 
-function App() {
+const App = () => {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/about" Component={About} />
         <Route path="/experience" Component={Experience} />
         <Route path="/projects" Component={Project} />
       </Routes>
-    </Router>
+    </>
   );
-}
+};
 
 export default App;
