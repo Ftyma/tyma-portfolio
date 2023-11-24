3dabcc13fd06ebdca1a44ce17aae8a7cdfb85e80
Featthima Bou <featthimabou@Featthimas-MacBook-Pro.local>
2023-11-24 18:49:56 +0700

    add icon

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -10,7 +10,8 @@ const projectList = [
   {
     title: " Amusigo",
     desc: "A web application that connect students through the same music taste",
-    icon: "fa-light fa-globe fa-brands fa-github",
+    icon: "fa-brands fa-github",
+    webIcon: "fa-light fa-globe fa-brands fa-github",
     url: `https://github.com/Ftyma/Amusigo`,
     stack: "PHP, HTML, CSS, MySQL",
   },
@@ -36,12 +37,12 @@ const Project = () => {
       <h1 className="font-comfortaa text-white text-4xl ml-10">Projects</h1>
 
       <div className="grid md:grid-cols-3 grid-cols-1 w-10/12 justify-items-center mx-auto">
-        {projectList.map(({ title, desc, icon, url, stack }) => (
+        {projectList.map(({ title, desc, icon, webIcon, url, stack }) => (
           <Card className="mt-16 w-80 shadow-2xl">
             <CardBody className="">
               <a href={url}>
                 <IconButton className="text-2xl bg-transparent text-black border-none shadow-none float-right">
-                  <i className={`text-pink ${icon}`} />
+                  <i className={`text-pink ${webIcon} ${icon}`} />
                 </IconButton>
               </a>
 
