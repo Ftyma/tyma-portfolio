9f32b12e16ea7e20365e8dcd86eec577f97f54dd
Featthima Bou <featthimabou@Featthimas-MacBook-Pro.local>
2023-11-24 18:53:09 +0700

    add website'

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -13,6 +13,7 @@ const projectList = [
     icon: "fa-brands fa-github",
     webIcon: "fa-light fa-globe fa-brands fa-github",
     url: `https://github.com/Ftyma/Amusigo`,
+    url1: `https://amusigo.000webhostapp.com/landing.php`,
     stack: "PHP, HTML, CSS, MySQL",
   },
   {
@@ -37,9 +38,14 @@ const Project = () => {
       <h1 className="font-comfortaa text-white text-4xl ml-10">Projects</h1>
 
       <div className="grid md:grid-cols-3 grid-cols-1 w-10/12 justify-items-center mx-auto">
-        {projectList.map(({ title, desc, icon, webIcon, url, stack }) => (
+        {projectList.map(({ title, desc, icon, webIcon, url, url1, stack }) => (
           <Card className="mt-16 w-80 shadow-2xl">
             <CardBody className="">
+              <a href={url1}>
+                <IconButton className="text-2xl bg-transparent text-black border-none shadow-none float-right">
+                  <i className={`text-pink ${webIcon} `} />
+                </IconButton>
+              </a>
               <a href={url}>
                 <IconButton className="text-2xl bg-transparent text-black border-none shadow-none float-right">
                   <i className={`text-pink ${webIcon} ${icon}`} />
